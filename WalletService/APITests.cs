87aac2c231c05e7cc29b5bd9222a79485a9c4e13
ashle1977 ashle1977 <pandeyanupam1977@rediffmail.com>
2024-11-07 10:39:23 +0000

    Add functionality to retrieve current balance from API using the /balance endpoint and defined the base URL in a separate configuration file for easier maintenance

---
+++ WalletService/APITests.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace WalletService
+{
+    public class BalanceResponse
+    {
+        public int Amount { get; set; }
+    }
+
+    public class ApiTests : BaseURL
+    {
+        private async Task<BalanceResponse> GetBalanceAsync()
+        {
+            var response = await Client.GetAsync("/onlinewallet/balance");
+            response.EnsureSuccessStatusCode();
+            var balanceContent = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<BalanceResponse>(balanceContent);
+        }
+
+        [Fact]
+        public async Task GetBalance_ShouldReturnCurrentBalance()
+        {
+            var balanceResponse = await GetBalanceAsync();
+            Assert.Equal(0, balanceResponse.Amount);
+        }
+    }
+}
---
+++ WalletService/BaseURL.cs
@@ -0,0 +1,17 @@
+﻿namespace WalletService
+{
+    public class BaseURL
+    {
+        protected readonly HttpClient Client;
+
+        public BaseURL()
+        {
+            Client = new HttpClient { BaseAddress = new Uri("http://localhost:5047") };
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
